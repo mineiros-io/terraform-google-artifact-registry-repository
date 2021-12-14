b41c2c6af649768ef44164068b3735d8e30094cd
Nathan Parraga Thiesen <nathan.thiesen@mineiros.io>
2021-12-14 10:43:07 +0100

    fix: set provider back to `google-beta`

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.14, < 2.0"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
+    google-beta = {
+      source  = "hashicorp/google-beta"
       version = ">= 4.0, < 5.0"
     }
   }
